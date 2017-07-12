e2e73318ff697fecb8cfdd98f385562bd998ea3a
AaronBruce <aaron.bruce@comcast.net>
2017-07-12 09:47:45 -0700

    Fixed html file calls, completed last two TODOs in the article.js file

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,7 +3,7 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
+  // DONE: Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
@@ -15,15 +15,16 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template.
+  /* DONE: This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
+  $($newArticle).removeClass().addClass(this.title);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest
   of the current template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -31,6 +32,11 @@ Article.prototype.toHtml = function() {
     3. article title,
     4. article body, and
     5. publication date. */
+    $('.template').find('h1').html(this.title);
+    $('.template').find('address').html(this.author);
+    $('.template').find('href').html(this.authorUrl);
+    $('.template').find('time').html(this.publishedOn);
+    $('.template').find('section').html(this.body);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
